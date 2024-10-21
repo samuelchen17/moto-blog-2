965f021512e738dcaa5f9aecaa2ea23c028e1688
Samuel Chen <sche842@gmail.com>
2024-10-21 20:04:35 +1100

    navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,5 +1,51 @@
+import {
+  Button,
+  Navbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarLink,
+  TextInput,
+} from "flowbite-react";
+import helmetIcon from "/helmet.svg";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const path = useLocation().pathname;
+
+  return (
+    <Navbar className="border-b-2">
+      <NavbarBrand as={Link} to="/">
+        <img src={helmetIcon} className="h-[60px] dark:invert" />
+        <span className="self-center whitespace-nowrap text-sm sm:text-4xl font-semibold dark:text-white">
+          SC MOTO
+        </span>
+      </NavbarBrand>
+
+      {/* search bar */}
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
+      </form>
+      <Button className="lg:hidden" color="gray">
+        <AiOutlineSearch />
+      </Button>
+
+      {/* links */}
+      <NavbarCollapse>
+        <NavbarLink as={Link} to="home" active={path === "/home"}>
+          HOME
+        </NavbarLink>
+        <NavbarLink as={Link} to="dashboard" active={path === "/dashboard"}>
+          DASH
+        </NavbarLink>
+      </NavbarCollapse>
+    </Navbar>
+  );
 };
 
 export default NavBar;
