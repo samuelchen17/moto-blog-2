f35cc6c5ca4e58ad83fa2799aa9703bc3fafe06f
Samuel Chen <sche842@gmail.com>
2024-10-24 19:12:58 +1100

    persist testing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,14 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { store } from "./redux/store.ts";
 import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor} loading={null}>
+      <App />
+    </PersistGate>
   </Provider>
 );
