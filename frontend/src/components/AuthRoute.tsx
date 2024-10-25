27ec873070cf76280e67abadb39fc74020e93eda
Samuel Chen <sche842@gmail.com>
2024-10-25 18:08:49 +1100

    auth route implemented

--- frontend/src/components/AuthRoute.tsx
+++ frontend/src/components/AuthRoute.tsx
@@ -1,12 +1,24 @@
-import { useAppSelector } from "../redux/hooks";
+import { useAppSelector, useAppDispatch } from "../redux/hooks";
+import { useEffect } from "react";
 import { RootState } from "../redux/store";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { toggleAuthModal } from "../redux/features/modal/authModalSlice";
 
 const AuthRoute = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   const { currentUser } = useAppSelector(
     (state: RootState) => state.persisted.user
   );
-  return currentUser ? <Outlet /> : <div>Implement redirect to sign in</div>;
+
+  useEffect(() => {
+    if (!currentUser) {
+      dispatch(toggleAuthModal()); // Dispatch your action here
+      navigate("/"); // Navigate after dispatching
+    }
+  }, [currentUser, dispatch, navigate]);
+
+  return currentUser ? <Outlet /> : null;
 };
 
 export default AuthRoute;
