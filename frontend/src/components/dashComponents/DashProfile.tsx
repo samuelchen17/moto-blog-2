4d30e8f51679c4f607c55ad23b76ad4ca3e23a42
Samuel Chen <sche842@gmail.com>
2024-10-27 16:08:17 +1100

    profile page adjustments

--- frontend/src/components/dashComponents/DashProfile.tsx
+++ frontend/src/components/dashComponents/DashProfile.tsx
@@ -8,46 +8,52 @@ const DashProfile = () => {
   );
 
   return (
-    <div className="max-w-lg outline-dotted w-full mx-auto">
-      <h1>Profile</h1>
-      <form className="flex flex-col">
-        <div className="self-center w-32 h-32">
-          <Avatar
-            alt="user"
-            className="rounded-full w-full h-full object-cover"
-            // img={currentUser.profilePicture} // implement
-          />
-        </div>
-        <div>
-          <div className="mb-2 block">
-            <Label htmlFor="username" value="Username" />
+    <div className="outline-dotted w-full mx-auto px-4">
+      <h1 className="text-2xl">Profile</h1>
+      <hr></hr>
+      <div className="flex flex-row">
+        <form className="flex flex-col">
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="username" value="Username" />
+            </div>
+            <TextInput
+              type="text"
+              id="username"
+              placeholder="username"
+              defaultValue={currentUser?.user.username}
+            />
           </div>
-          <TextInput
-            type="text"
-            id="username"
-            placeholder="username"
-            defaultValue={currentUser?.user.username}
-          />
-        </div>
 
-        <div>
-          <div className="mb-2 block">
-            <Label htmlFor="email" value="Email" />
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="email" value="Email" />
+            </div>
+            <TextInput
+              type="text"
+              id="email"
+              placeholder="email"
+              defaultValue={currentUser?.user.email}
+            />
           </div>
-          <TextInput
-            type="text"
-            id="email"
-            placeholder="email"
-            defaultValue={currentUser?.user.email}
-          />
-        </div>
-        <div>
-          <div className="mb-2 block">
-            <Label htmlFor="password" value="Your password" />
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="password" value="Your password" />
+            </div>
+            <TextInput type="text" id="password" placeholder="password" />
           </div>
-          <TextInput type="text" id="password" placeholder="password" />
+        </form>
+        <div>
+          <span>Profile picture</span>
+          <div className="self-center w-32 h-32">
+            <Avatar
+              alt="user"
+              className="rounded-full w-full h-full object-cover"
+              // img={currentUser.profilePicture} // implement
+            />
+          </div>
         </div>
-      </form>
+      </div>
       <div>
         <span>Delete Account</span>
       </div>
