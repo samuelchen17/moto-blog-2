1e0f98680f08aabae8a1886495a1de1f33728d64
Samuel Chen <sche842@gmail.com>
2024-10-27 14:21:02 +1100

    dash profile

--- frontend/src/components/dashComponents/DashProfile.tsx
+++ frontend/src/components/dashComponents/DashProfile.tsx
@@ -1,5 +1,44 @@
+import { RootState } from "../../redux/store";
+import { useAppSelector } from "../../redux/hooks";
+import { Avatar, TextInput } from "flowbite-react";
+
 const DashProfile = () => {
-  return <div>DashProfile</div>;
+  const { currentUser } = useAppSelector(
+    (state: RootState) => state.persisted.user
+  );
+
+  return (
+    <div className="max-w-lg mx-auto">
+      <h1>Profile</h1>
+      <form className="flex flex-col">
+        <div className="self-center w-32 h-32">
+          <Avatar
+            alt="user"
+            className="rounded-full w-full h-full object-cover"
+            // img={currentUser.profilePicture} // implement
+          />
+        </div>
+        <TextInput
+          type="text"
+          id="username"
+          placeholder="username"
+          defaultValue={currentUser?.user.username}
+        />
+        <TextInput
+          type=""
+          id="username"
+          placeholder="username"
+          defaultValue={currentUser?.user.username}
+        />
+        <TextInput
+          type="text"
+          id="username"
+          placeholder="username"
+          defaultValue={currentUser?.user.username}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default DashProfile;
--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import DashProfile from "../components/dashComponents/DashProfile";
 import DashSidebar from "../components/dashComponents/DashSidebar";
+import { Avatar } from "flowbite-react";
 
 const DashboardPage = () => {
   const location = useLocation();
@@ -19,6 +20,13 @@ const DashboardPage = () => {
     <div className="min-h-screen flex flex-col md:flex-row">
       {/* sidebar */}
       <div className="">
+        <div className="flex flex-row">
+          <Avatar rounded />
+          <div className="flex flex-col">
+            <span>Username</span>
+            <span>User Account</span>
+          </div>
+        </div>
         <DashSidebar />
       </div>
       {/* profile */}
