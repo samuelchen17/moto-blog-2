4a68391420a84780dfb290ac92f9a4bc43bad09d
Samuel Chen <sche842@gmail.com>
2024-10-27 15:01:38 +1100

    profile page prototype

--- frontend/src/components/dashComponents/DashProfile.tsx
+++ frontend/src/components/dashComponents/DashProfile.tsx
@@ -1,6 +1,6 @@
 import { RootState } from "../../redux/store";
 import { useAppSelector } from "../../redux/hooks";
-import { Avatar, TextInput } from "flowbite-react";
+import { Avatar, Label, TextInput } from "flowbite-react";
 
 const DashProfile = () => {
   const { currentUser } = useAppSelector(
@@ -18,25 +18,39 @@ const DashProfile = () => {
             // img={currentUser.profilePicture} // implement
           />
         </div>
-        <TextInput
-          type="text"
-          id="username"
-          placeholder="username"
-          defaultValue={currentUser?.user.username}
-        />
-        <TextInput
-          type=""
-          id="username"
-          placeholder="username"
-          defaultValue={currentUser?.user.username}
-        />
-        <TextInput
-          type="text"
-          id="username"
-          placeholder="username"
-          defaultValue={currentUser?.user.username}
-        />
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="username" value="Username" />
+          </div>
+          <TextInput
+            type="text"
+            id="username"
+            placeholder="username"
+            defaultValue={currentUser?.user.username}
+          />
+        </div>
+
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="email" value="Email" />
+          </div>
+          <TextInput
+            type="text"
+            id="email"
+            placeholder="email"
+            defaultValue={currentUser?.user.email}
+          />
+        </div>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="password" value="Your password" />
+          </div>
+          <TextInput type="text" id="password" placeholder="password" />
+        </div>
       </form>
+      <div>
+        <span>Delete Account</span>
+      </div>
     </div>
   );
 };
