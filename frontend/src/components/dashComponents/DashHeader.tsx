efb3bc2b854dcb937d7faf7deb0a7b9b9b1fabfd
Samuel Chen <sche842@gmail.com>
2024-10-28 19:32:46 +1100

    dp implemented

--- frontend/src/components/dashComponents/DashHeader.tsx
+++ frontend/src/components/dashComponents/DashHeader.tsx
@@ -1,9 +1,15 @@
 import { Avatar } from "flowbite-react";
+import { useAppSelector } from "../../redux/hooks";
+import { RootState } from "../../redux/store";
 
 const DashHeader = () => {
+  const { currentUser } = useAppSelector(
+    (state: RootState) => state.persisted.user
+  );
+
   return (
     <div className="flex flex-row gap-2 mx-4 my-4">
-      <Avatar rounded />
+      <Avatar rounded img={currentUser?.user.profilePicture} />
       <div className="flex flex-col gap">
         <span className="text-lg font-semibold">Username</span>
         <span className="text-sm">User Account</span>
