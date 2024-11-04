c3a95185d5fd75e5b981457e88ff048f66639ac0
Samuel Chen <sche842@gmail.com>
2024-11-04 14:14:20 +1100

    post controller

--- backend/src/controllers/post.controller.ts
+++ backend/src/controllers/post.controller.ts
@@ -1,7 +1,22 @@
 import { Request, Response, NextFunction } from "express";
+import { CustomError } from "../utils/errorHandler.utils";
 
 export const createPost = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  try {
+    if (!req.body.title || !req.body.content) {
+      next(new CustomError(400, "Please fill out all required fields"));
+    }
+
+    const slug = req.body.title
+      .split("")
+      .join("-")
+      .toLowerCase()
+      .replace(/[^a-zA-Z0-9-]/g, "-");
+  } catch (err) {
+    next(new CustomError(500, "Failed to create post"));
+  }
+};
