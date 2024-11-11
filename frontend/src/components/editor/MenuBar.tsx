d42ccc49d0933416bc31b60155981309ea8e29e3
Samuel Chen <sche842@gmail.com>
2024-11-11 18:14:35 +1100

    tiptap menu button fix

--- frontend/src/components/editor/MenuBar.tsx
+++ frontend/src/components/editor/MenuBar.tsx
@@ -22,24 +22,28 @@ const MenuBar = ({ editor }: IMenuBar) => {
   return (
     <div className="border rounded-md flex items-center flex-wrap gap-x-4 p-4">
       <button
+        type="button"
         onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
         className={editor.isActive("heading", { level: 1 }) ? "is-active" : ""}
       >
         <BsTypeH1 size={menuItemSize} />
       </button>
       <button
+        type="button"
         onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
         className={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}
       >
         <BsTypeH2 size={menuItemSize} />
       </button>
       <button
+        type="button"
         onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
         className={editor.isActive("heading", { level: 3 }) ? "is-active" : ""}
       >
         <BsTypeH3 size={menuItemSize} />
       </button>
       <button
+        type="button"
         onClick={() => editor.chain().focus().toggleBold().run()}
         disabled={!editor.can().chain().focus().toggleBold().run()}
         className={editor.isActive("bold") ? "is-active" : ""}
@@ -47,6 +51,7 @@ const MenuBar = ({ editor }: IMenuBar) => {
         <MdFormatBold size={menuItemSize} />
       </button>
       <button
+        type="button"
         onClick={() => editor.chain().focus().toggleItalic().run()}
         disabled={!editor.can().chain().focus().toggleItalic().run()}
         className={editor.isActive("italic") ? "is-active" : ""}
@@ -54,12 +59,14 @@ const MenuBar = ({ editor }: IMenuBar) => {
         <MdFormatItalic size={menuItemSize} />
       </button>
       <button
+        type="button"
         onClick={() => editor.chain().focus().toggleUnderline().run()}
         className={editor.isActive("underline") ? "is-active" : ""}
       >
         <MdFormatUnderlined size={menuItemSize} />
       </button>
       <button
+        type="button"
         onClick={() => editor.chain().focus().toggleStrike().run()}
         disabled={!editor.can().chain().focus().toggleStrike().run()}
         className={editor.isActive("strike") ? "is-active" : ""}
@@ -68,12 +75,14 @@ const MenuBar = ({ editor }: IMenuBar) => {
       </button>
 
       <button
+        type="button"
         onClick={() => editor.chain().focus().undo().run()}
         disabled={!editor.can().chain().focus().undo().run()}
       >
         Undo
       </button>
       <button
+        type="button"
         onClick={() => editor.chain().focus().redo().run()}
         disabled={!editor.can().chain().focus().redo().run()}
       >
@@ -92,7 +101,7 @@ const MenuBar = ({ editor }: IMenuBar) => {
       >
         Clear nodes
       </button>
-      {/* <button
+      {/* <button type="button"
         onClick={() => editor.chain().focus().setParagraph().run()}
         className={editor.isActive("paragraph") ? "is-active" : ""}
       >
@@ -100,18 +109,21 @@ const MenuBar = ({ editor }: IMenuBar) => {
       </button> */}
 
       <button
+        type="button"
         onClick={() => editor.chain().focus().toggleBulletList().run()}
         className={editor.isActive("bulletList") ? "is-active" : ""}
       >
         Bullet list
       </button>
       <button
+        type="button"
         onClick={() => editor.chain().focus().toggleOrderedList().run()}
         className={editor.isActive("orderedList") ? "is-active" : ""}
       >
         Ordered list
       </button>
       <button
+        type="button"
         onClick={() => editor.chain().focus().toggleBlockquote().run()}
         className={editor.isActive("blockquote") ? "is-active" : ""}
       >
--- implement.md
+++ implement.md
@@ -2,3 +2,6 @@ react hook form?
 shad cn form?
 add axios?
 delete dp image if not submitted - needs firebase auth, configure
+
+add AI to check if post is appropriate?
+add ai to summarise post?
