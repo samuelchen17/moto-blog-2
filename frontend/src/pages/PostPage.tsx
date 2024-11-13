a18790da6293eb0023a331e4241d4c8bd0a2a78e
Samuel Chen <sche842@gmail.com>
2024-11-13 18:13:06 +1100

    connect display post to api

--- frontend/src/pages/PostPage.tsx
+++ frontend/src/pages/PostPage.tsx
@@ -1,12 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const PostPage = () => {
   const { postSlug } = useParams();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
+  const [post, setPost] = useState(null);
 
   useEffect(() => {
-    console.log(postSlug);
+    const fetchPost = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/post/getposts?slug=${postSlug}`);
+        const data = await res.json();
+
+        if (!res.ok) {
+          setLoading(false);
+          setError(true);
+          throw new Error("Failed response");
+        }
+
+        setPost(data.posts[0]);
+        setLoading(false);
+        setError(false);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchPost();
   }, [postSlug]);
+
+  console.log(post);
   return <div>PostPage</div>;
 };
 
