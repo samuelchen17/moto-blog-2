03fcccdb381b61cdaf0f5eade08f7669eaba1bc7
Samuel Chen <sche842@gmail.com>
2024-11-15 21:31:40 +1100

    post comment router setup

--- backend/src/router/index.router.ts
+++ backend/src/router/index.router.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import authRouter from "./auth.router";
 import userRouter from "./user.router";
 import postRouter from "./post.router";
+import commentRouter from "./comment.router";
 
 const router: Router = Router();
 
@@ -9,5 +10,6 @@ export default (): Router => {
   authRouter(router);
   userRouter(router);
   postRouter(router);
+  commentRouter(router);
   return router;
 };
