af2934c23c5563090c8b9e83ae94d23d65bcab19
Samuel Chen <sche842@gmail.com>
2024-11-18 23:11:47 +1100

    get commentby frontend

--- frontend/src/components/commentSection/CommentSectionComment.tsx
+++ frontend/src/components/commentSection/CommentSectionComment.tsx
@@ -1,6 +1,27 @@
 import { IComment } from "@shared/types/comment";
+import { IGetUser } from "@shared/types/user";
+import { useEffect, useState } from "react";
 
 const CommentSectionComment = ({ comment }: { comment: IComment }) => {
+  const [commentBy, setCommentBy] = useState<IGetUser | null>(null);
+
+  console.log(commentBy);
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const res = await fetch(`/api/${comment.commentBy}`);
+        if (res.ok) {
+          const data: IGetUser = await res.json();
+          setCommentBy(data);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getUser();
+  }, [comment]);
   return <div>{comment.content}</div>;
 };
 
