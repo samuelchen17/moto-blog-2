6643e99ad261b6bb1dff7c6279d5bc9d6672edaa
Samuel Chen <sche842@gmail.com>
2024-11-21 19:19:18 +1100

    added author to recent post card

--- frontend/src/components/recentPosts/RecentPostCard.tsx
+++ frontend/src/components/recentPosts/RecentPostCard.tsx
@@ -1,7 +1,30 @@
 import { IPost } from "@shared/types/post";
+import { IGetUser } from "@shared/types/user";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const RecentPostCard = ({ post }: { post: IPost }) => {
+  const [author, setAuthor] = useState<IGetUser | null>(null);
+
+  useEffect(() => {
+    try {
+      const fetchAuthor = async () => {
+        const res = await fetch(`/api/${post.createdBy}`);
+
+        const data: IGetUser = await res.json();
+
+        if (res.ok) {
+          setAuthor(data);
+        }
+      };
+
+      fetchAuthor();
+    } catch (err) {
+      console.error(err);
+    }
+  }, [post]);
+
+  console.log(author);
   return (
     <div className="">
       <Link to={`/blogs/post/${post.slug}`}>
@@ -10,8 +33,16 @@ const RecentPostCard = ({ post }: { post: IPost }) => {
           alt={post.title}
           className="h-[260px] w-full object-cover"
         />
-        <span>{post.title}</span>
       </Link>
+      {/* implement link to authors page */}
+      <div className="flex items-center gap-2">
+        <img
+          src={author?.profilePicture}
+          className="h-5 w-5 object-cover rounded-full bg-blue-500"
+        />
+        <span className="font-bold">{author?.username}</span>
+      </div>
+
       <div>
         <p>{post.title}</p>
         <span>{post.category}</span>
--- frontend/src/components/recentPosts/RecentPosts.tsx
+++ frontend/src/components/recentPosts/RecentPosts.tsx
@@ -25,7 +25,7 @@ const RecentPosts = () => {
   console.log(recentPosts);
 
   return (
-    <div className="flex flex-col md:flex-row justify-between ">
+    <div className="grid lg:grid-cols-3">
       {recentPosts &&
         recentPosts.map((post) => (
           <RecentPostCard key={post._id} post={post} />
