9d31f5d70c186be4e91b1e378dbf89c07bfffd21
Samuel Chen <sche842@gmail.com>
2024-11-24 20:25:03 +1100

    dashboard dash

--- frontend/src/components/dashComponents/dashAdminComponents/Dashboard.tsx
+++ frontend/src/components/dashComponents/dashAdminComponents/Dashboard.tsx
@@ -1,6 +1,22 @@
-import React from "react";
+import { useState } from "react";
+import { useAppSelector } from "../../../redux/hooks";
+import { RootState } from "../../../redux/store";
 
 const Dashboard = () => {
+  const [users, setUsers] = useState([]);
+  const [comments, setComments] = useState([]);
+  const [post, setPosts] = useState([]);
+  const [totalUsers, setTotalUsers] = useState(0);
+  const [totalComments, setTotalComments] = useState(0);
+  const [totalPosts, setTotalPosts] = useState(0);
+  const [lastMonthUsers, setLastMonthUsers] = useState(0);
+  const [lastMonthComments, setLastMonthComments] = useState(0);
+  const [lastMonthPosts, setLastMonthPosts] = useState(0);
+
+  const { currentUser } = useAppSelector(
+    (state: RootState) => state.persisted.user
+  );
+
   return <div>Dashboard</div>;
 };
 
