6c96d7fb50f3a55b2b571749e7f093c3033df801
Samuel Chen <sche842@gmail.com>
2024-11-26 14:37:02 +1100

    shadcn setup

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,23 +3,59 @@ const typography = require("@tailwindcss/typography");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
-    extend: {
-      colors: { background: " #121212" },
-      typography: ({ theme }) => ({
-        DEFAULT: {
-          css: {
-            "--tw-prose-bullets": theme("colors.pink[400]"),
-            li: {
-              p: {
-                margin: 0,
-              },
-            },
-          },
-        },
-      }),
-    },
+  	extend: {
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		typography: '({ theme }) => ({\n        DEFAULT: {\n          css: {\n            "--tw-prose-bullets": theme("colors.pink[400]"),\n            li: {\n              p: {\n                margin: 0,\n              },\n            },\n          },\n        },\n      })',
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [flowbite.plugin(), typography],
+  plugins: [flowbite.plugin(), typography, require("tailwindcss-animate")],
 };
