09938183a4893c2788e42ceaa23751795b1f0ac3
Samuel Chen <sche842@gmail.com>
2024-12-05 18:56:18 +1100

    dropdown ui changed

--- frontend/src/components/navBar/NavBarAuth.tsx
+++ frontend/src/components/navBar/NavBarAuth.tsx
@@ -1,4 +1,5 @@
-import { Avatar, Button, Dropdown, Navbar } from "flowbite-react";
+import { Avatar, Dropdown, Navbar } from "flowbite-react";
+import { Button } from "../ui/button";
 import AuthModal from "../authModal/AuthModal";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { RootState } from "../../redux/store";
@@ -11,6 +12,17 @@ import {
 import { dashNavItems } from "../../config/dashNavItems.config";
 import userSignOut from "../../utils/userSignOut.utils";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogOut, User } from "lucide-react";
+
 const NavBarAuth = () => {
   const { currentUser } = useAppSelector(
     (state: RootState) => state.persisted.user
@@ -22,50 +34,95 @@ const NavBarAuth = () => {
   return (
     <>
       {currentUser ? (
-        <Dropdown
-          arrowIcon={false}
-          inline
-          label={
-            <Avatar alt="user" rounded img={currentUser.user.profilePicture} />
-          }
-        >
-          <Dropdown.Header>
-            <span className="block text-sm">{currentUser.user.username}</span>
-            <span className="block truncate text-sm font-medium">
-              {currentUser.user.email}
-            </span>
-          </Dropdown.Header>
+        <>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Avatar
+                alt="user"
+                rounded
+                img={currentUser.user.profilePicture}
+              />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-44">
+              <DropdownMenuLabel>
+                <span className="block text-sm">
+                  {currentUser.user.username}
+                </span>
+                <span className="block truncate text-sm font-medium">
+                  {currentUser.user.email}
+                </span>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  <User />
+                  <span>Profile</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>Billing</DropdownMenuItem>
+                <DropdownMenuItem>Team</DropdownMenuItem>
+                <DropdownMenuItem>Subscription</DropdownMenuItem>
+              </DropdownMenuGroup>
 
-          {dashNavItems
-            .filter((item) => !item.admin || currentUser?.user.admin)
-            .map((item) => (
-              <Dropdown.Item
-                key={item.name}
-                as={Link}
-                to={item.path}
-                icon={item.icon}
-                className="capitalize"
-              >
-                {item.name}
-              </Dropdown.Item>
-            ))}
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <LogOut />
+                <span>Sign Out</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
 
-          <Dropdown.Divider />
-          <Dropdown.Item
-            icon={PiSignOutBold}
-            onClick={() => userSignOut({ dispatch })}
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar
+                alt="user"
+                rounded
+                img={currentUser.user.profilePicture}
+              />
+            }
           >
-            Sign Out
-          </Dropdown.Item>
-        </Dropdown>
+            <Dropdown.Header>
+              <span className="block text-sm">{currentUser.user.username}</span>
+              <span className="block truncate text-sm font-medium">
+                {currentUser.user.email}
+              </span>
+            </Dropdown.Header>
+
+            {dashNavItems
+              .filter((item) => !item.admin || currentUser?.user.admin)
+              .map((item) => (
+                <Dropdown.Item
+                  key={item.name}
+                  as={Link}
+                  to={item.path}
+                  icon={item.icon}
+                  className="capitalize"
+                >
+                  {item.name}
+                </Dropdown.Item>
+              ))}
+
+            <Dropdown.Divider />
+            <Dropdown.Item
+              icon={PiSignOutBold}
+              onClick={() => userSignOut({ dispatch })}
+            >
+              Sign Out
+            </Dropdown.Item>
+          </Dropdown>
+        </>
       ) : (
         <>
-          <Button color="none" onClick={() => dispatch(openLogin())}>
+          <Button
+            variant="ghost"
+            className="rounded-full"
+            onClick={() => dispatch(openLogin())}
+          >
             Log In
           </Button>
           <Button
-            className="bg-black dark:bg-white"
-            pill
+            className="rounded-full"
             onClick={() => dispatch(openRegister())}
           >
             Sign Up
