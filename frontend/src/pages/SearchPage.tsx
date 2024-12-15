81ea19f7e551111cc0c3606ddab620f5aea43811
Samuel Chen <sche842@gmail.com>
2024-12-16 00:09:39 +1100

    search item ui

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -39,7 +39,16 @@ const SearchPage = () => {
           </Select>
         </div>
 
-        <div>content</div>
+        {/* search */}
+        <div>
+          <div className="mt-12 flex gap-2 items-center mb-5">
+            <div className="bg-red-500 h-5 w-5 rounded-full" />
+            <span className="text-sm">Guest Author</span>
+          </div>
+
+          <h2 className="text-xl font-bold mb-1">Ai is killing coding</h2>
+          <p className="text-gray-500">Content of the post, goes here ...</p>
+        </div>
       </div>
     </div>
   );
