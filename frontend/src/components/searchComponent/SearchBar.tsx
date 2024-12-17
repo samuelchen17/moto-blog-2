f994963a6f9fe3e99628e8f4cad99913c8e04849
Samuel Chen <sche842@gmail.com>
2024-12-17 18:56:47 +1100

    search bar prototype

--- frontend/src/components/searchComponent/SearchBar.tsx
+++ frontend/src/components/searchComponent/SearchBar.tsx
@@ -1,14 +1,46 @@
 import { Button } from "../ui/button";
 import { Search } from "lucide-react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Input } from "../ui/input";
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  console.log(searchTerm);
+
+  // useEffect to get search term from url
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set("searchTerm", searchTerm);
+
+    navigate(`/search?${urlParams.toString()}`);
+  };
+
   return (
-    <Button className="rounded-full">
-      <Link to="/search">
-        <Search />
-      </Link>
-    </Button>
+    // <Button className="rounded-full">
+    //   <Link to="/search">
+    //     <Search />
+    //   </Link>
+    // </Button>
+    <form onSubmit={handleSubmit}>
+      <Input
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+    </form>
   );
 };
 
