b351c16049e73f11323f95d717ccebd52a1cb638
Samuel Chen <sche842@gmail.com>
2024-12-25 01:26:31 +1100

    manual import env for vite config fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+
+// this line is needed to use env files in vite.config file
+// need to manually load env
+const env = loadEnv(process.env.NODE_ENV as string, process.cwd(), "VITE_");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: process.env.VITE_API_URL || "http://localhost:3000", //implement prod, change localhost to dev
-        secure: process.env.NODE_ENV === "production",
+        target: env.VITE_API_URL, //implement prod, change localhost to dev
+        secure: env.NODE_ENV === "production",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""), // remove "/api" before forward
       },
