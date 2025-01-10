5b6bbd8f6c47f6f24c53022a42a8bc8b5ce3d3b8
Samuel Chen <sche842@gmail.com>
2025-01-10 17:40:21 +1100

    summarize component testing

--- frontend/src/components/Summarizer.tsx
+++ frontend/src/components/Summarizer.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import summarizeText from "@/utils/huggingFaceAi";
 
 const Summarizer = () => {
-  return <div>Summarizer</div>;
+  const [summarizedText, setSummarizedText] = useState<any>();
+
+  const text = "this needs to be summarised";
+
+  const handleSummarize = () => {
+    const summary = summarizeText(text);
+    if (summary) {
+      setSummarizedText(summary);
+    } else {
+      console.log("Failed to summarize");
+    }
+  };
+
+  return (
+    <div>
+      <Button onClick={handleSummarize}>Summarize</Button>
+      {summarizedText ? (
+        <div>{summarizedText}</div>
+      ) : (
+        <div>Click the button</div>
+      )}
+    </div>
+  );
 };
 
 export default Summarizer;
--- frontend/src/utils/huggingFaceAi.ts
+++ frontend/src/utils/huggingFaceAi.ts
@@ -4,10 +4,18 @@ const inference = new HfInference(
   import.meta.env.VITE_HUGGINGFACE_ACCESS_TOKEN
 );
 
-const result = await inference.textClassification({
-  model: "facebook/bart-large-cnn",
-  inputs:
-    "Today is a great day, there was rain but yet there was sun, so overall it was quite a good day",
-});
+const summarizeText = async (text: string) => {
+  try {
+    const result = await inference.textClassification({
+      model: "facebook/bart-large-cnn",
+      inputs: text,
+    });
 
-console.log(result);
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export default summarizeText;
