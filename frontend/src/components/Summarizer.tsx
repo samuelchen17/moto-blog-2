04028920f35c677c3fe449091c0fdef7c18c04e8
Samuel Chen <sche842@gmail.com>
2025-01-11 14:29:14 +1100

    loading spinner refactor

--- frontend/src/components/Summarizer.tsx
+++ frontend/src/components/Summarizer.tsx
@@ -1,29 +1,10 @@
 import { useState } from "react";
 import { Button } from "./ui/button";
 import summarizeText from "@/utils/huggingFaceAi";
-import { cn } from "@/lib/utils";
+import { LoadingSpinner } from "./LoadingSpinner";
 
 const stripHtml = (html: any) => {
   return html.replace(/<\/?[^>]+(>|$)/g, "");
-};
-
-export const LoadingSpinner = ({ className }: any) => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={cn("animate-spin", className)}
-    >
-      <path d="M21 12a9 9 0 1 1-6.219-8.56" />
-    </svg>
-  );
 };
 
 const Summarizer = ({ text }: { text: string }) => {
