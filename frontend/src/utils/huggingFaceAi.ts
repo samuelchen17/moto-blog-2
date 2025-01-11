535b3ff875c7c696e070bb0bac2e8231292ede70
Samuel Chen <sche842@gmail.com>
2025-01-11 16:14:33 +1100

    summarizer deployment

--- frontend/src/utils/huggingFaceAi.ts
+++ frontend/src/utils/huggingFaceAi.ts
@@ -4,23 +4,38 @@ const inference = new HfInference(
   import.meta.env.VITE_HUGGINGFACE_ACCESS_TOKEN
 );
 
+const generateRandVal = (min: number, max: number) =>
+  Math.random() * (max - min) + min;
+
+const parameters = {
+  max_length: 250, // max tokens in output
+  min_length: 150,
+  temperature: generateRandVal(0.3, 0.6), // randomness, 0 = more factual, 1 = more creative
+  top_k: Math.floor(generateRandVal(30, 50)), // lower ensure relevance, higher adds diversity
+  top_p: 0.9, // allows flexibility, balancing, diversity and relevance
+};
+
+// const parameters = {
+//   max_length: 250, // max tokens in output
+//   min_length: 150,
+//   temperature: 0.6, // randomness, 0 = more factual, 1 = more creative
+//   top_k: 40, // lower ensure relevance, higher adds diversity
+//   top_p: 0.9, // allows flexibility, balancing, diversity and relevance
+// };
+
 const summarizeText = async (text: string) => {
   try {
     // console.log(text);
-    const prompt = `Summarize: \n\n${text}`;
+    const prompt = `Summarize this text into concise points with bullet lists: \n\n${text}`;
 
     const result = await inference.summarization({
       model: "facebook/bart-large-cnn",
       // model: "t5-base",
       inputs: prompt,
-      parameters: {
-        max_length: 250, // max tokens in output
-        min_length: 200,
-        temperature: 0.4, // randomness, 0 = more factual, 1 = more creative
-        top_k: 30, // lower ensure relevance, higher adds diversity
-        top_p: 0.9, // allows flexibility, balancing, diversity and relevance
-      },
+      parameters,
     });
+
+    console.log(parameters);
 
     return result.summary_text;
   } catch (err) {
