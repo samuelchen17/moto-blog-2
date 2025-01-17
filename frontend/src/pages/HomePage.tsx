e2bbf75fecf29b3370e98e53523eec106fa90120
Samuel Chen <sche842@gmail.com>
2025-01-17 16:25:10 +1100

    contact us ui complete

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -6,6 +6,7 @@ import UpcomingEvents from "@/components/events/UpcomingEvents";
 import HeadingTwoWrapper from "@/components/HeadingTwoWrapper";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
 
 const HomePage = () => {
   return (
@@ -44,40 +45,26 @@ const HomePage = () => {
         {/* contact us */}
         <div>
           <HeadingTwoWrapper>Contact us</HeadingTwoWrapper>
-          <div className="flex justify-between p-4 border rounded-md relative h-[400px]">
+          <div className="flex justify-between overflow-hidden border rounded-md relative h-[600px]">
             <img
-              className="w-full h-full object-cover filter contrast-125 brightness-75"
+              className="absolute w-full h-full object-cover filter contrast-125 brightness-75 top-0 left-0"
               alt="group ride image"
               src="https://images.unsplash.com/photo-1690540293122-14d3051a5fe5?q=80&w=2698&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             />
-            <div className="absolute flex w-full">
-              <div className="flex-grow text-white">
-                <span>We are here to help</span>
-                <p>
-                  Please leave us a message and we will get back to you within
-                </p>
-              </div>
-              <div className="flex-grow space-y-4 rounded-md p-4 bg-black">
-                <Input placeholder="name" />
-                <Input placeholder="email" type="email" />
-                <Textarea />
-                <Button variant="secondary">Send message</Button>
-              </div>
-            </div>
-          </div>
-
-          <div
-            className="relative bg-cover bg-center h-[400px] rounded-md border filter contrast-125 brightness-75"
-            style={{
-              backgroundImage:
-                "url('https://images.unsplash.com/photo-1690540293122-14d3051a5fe5?q=80&w=2698&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
-            }}
-          >
-            <div className="absolute inset-0 flex justify-between p-8">
-              <div className="flex-grow space-y-4 rounded-md p-6 bg-black my-auto">
-                <Input placeholder="name" />
-                <Input placeholder="email" type="email" />
-                <Textarea />
+            <div className="absolute bottom-0 top-0 outline flex flex-col md:flex-row w-full justify-end">
+              <div className="flex-grow space-y-6 rounded-md p-10 bg-black bg-opacity-90 md:my-auto md:m-10 m-4 max-w-[600px]">
+                <div>
+                  <Label className="text-white">Name</Label>
+                  <Input placeholder="name" />
+                </div>
+                <div>
+                  <Label className="text-white">Email</Label>
+                  <Input placeholder="email" type="email" />
+                </div>
+                <div>
+                  <Label className="text-white">Message</Label>
+                  <Textarea />
+                </div>
                 <Button variant="secondary">Send message</Button>
               </div>
             </div>
