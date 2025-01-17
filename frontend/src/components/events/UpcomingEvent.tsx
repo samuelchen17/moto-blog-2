e40479c4e24ed193f9d785886d06ed2a219145ec
Samuel Chen <sche842@gmail.com>
2025-01-17 17:25:50 +1100

    push to main

--- frontend/src/components/events/UpcomingEvent.tsx
+++ frontend/src/components/events/UpcomingEvent.tsx
@@ -8,13 +8,15 @@ const UpcomingEvent = ({ event }: { event: IEvent }) => {
     // finish hot posts selection? or perhaps integrate algo to auto select hot posts - might be too resource intensive for free service
 
     <div className="flex gap-4 rounded-md sm:dark:bg-transparent border sm:border-none sm:bg-transparent mx-auto sm:p-0 p-4 w-full">
-      <div className="py-4 px-6 rounded-md border mb-auto hidden sm:block text-center mr-2">
+      {/* date card */}
+      <div className="py-4 px-6 rounded-md mb-auto hidden sm:block text-center mr-2 bg-secondary">
         <div className="font-bold text-blue-500 uppercase">
           {format(event.date, "MMM")}
         </div>
         <div className="text-2xl font-bold">{format(event.date, "dd")}</div>
       </div>
 
+      {/* event content */}
       <div className="gap-2 flex flex-col">
         <div className="">
           <span className="text-blue-500 uppercase font-semibold">
@@ -28,6 +30,7 @@ const UpcomingEvent = ({ event }: { event: IEvent }) => {
 
         <p className="mb-6 sm:mb-0 line-clamp-3">{event.description}</p>
 
+        {/* join event button */}
         <div className="flex sm:flex-row flex-col gap-4 sm:items-center">
           <Button className="order-1 sm:order-none">Join</Button>
           <div className="flex gap-1">
