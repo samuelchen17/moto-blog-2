84d38709bf7db6e87e8c9a8938ae930b8028d574
Samuel Chen <sche842@gmail.com>
2025-01-19 16:47:52 +1100

    comment like save added to post page

--- frontend/src/pages/PostPage.tsx
+++ frontend/src/pages/PostPage.tsx
@@ -9,6 +9,8 @@ import TableOfContents from "@/components/TableOfContents";
 import { _get } from "@/api/axiosClient";
 import Summarizer from "@/components/Summarizer";
 import { SkeletonPostPage } from "@/components/SkeletonComponents";
+import { Bookmark, MessageSquare, ThumbsUp } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
 
 const PostPage = () => {
   const { postSlug } = useParams();
@@ -128,6 +130,43 @@ const PostPage = () => {
             <TableOfContents toc={tableOfContents} />
 
             <main className="w-full">
+              <div className="space-y-4 mb-12">
+                <Separator />
+                <div className="flex justify-between">
+                  {/* like comment and saves */}
+                  <div className="flex gap-6">
+                    <div className="flex items-center text-center gap-1">
+                      <ThumbsUp className="w-5 h-5" />
+                      <span className="text-xs">{post.likes}</span>
+                    </div>
+                    <div className="flex items-center text-center gap-1">
+                      <MessageSquare className="w-5 h-5" />
+                      <span className="text-xs">{post.comments}</span>
+                    </div>
+                    <div className="flex items-center text-center gap-1">
+                      <Bookmark className="w-5 h-5" />
+                      <span className="text-xs">{post.saves}</span>
+                    </div>
+                  </div>
+
+                  {/* date posted/updated */}
+                  <div className="font-semibold uppercase">
+                    {post.createdAt === post.updatedAt ? (
+                      <div>
+                        Posted:{" "}
+                        {format(new Date(post.createdAt), "dd MMM yyyy")}
+                      </div>
+                    ) : (
+                      <div>
+                        Updated:{" "}
+                        {format(new Date(post.updatedAt), "dd MMM yyyy")}
+                      </div>
+                    )}
+                  </div>
+                </div>
+                <Separator />
+              </div>
+
               <div
                 className="post-content"
                 dangerouslySetInnerHTML={{ __html: post.content }}
