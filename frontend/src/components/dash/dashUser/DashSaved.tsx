b74832c7f8cf7addfd914972a6a56416633af880
Samuel Chen <sche842@gmail.com>
2025-01-20 17:48:08 +1100

    fetch user saved posts in dash

--- frontend/src/components/dash/dashUser/DashSaved.tsx
+++ frontend/src/components/dash/dashUser/DashSaved.tsx
@@ -1,5 +1,35 @@
+import { _get } from "@/api/axiosClient";
+import { useAppSelector } from "@/redux/hooks";
+import { RootState } from "@/redux/store";
+import { IGetUser } from "@/types";
+import { useEffect, useState } from "react";
+
 const DashSaved = () => {
-  return <div>DashSaved</div>;
+  const { currentUser } = useAppSelector(
+    (state: RootState) => state.persisted.user
+  );
+
+  const [savedList, setSavedList] = useState();
+  // useEffect to get users saved list
+
+  useEffect(() => {
+    if (currentUser) {
+      const fetchSavedList = async () => {
+        try {
+          const res = await _get<IGetUser>(`/${currentUser.user.id}`);
+          const data = res.data;
+
+          console.log(data);
+        } catch (err) {
+          console.error(err);
+        }
+      };
+
+      fetchSavedList();
+    }
+  }, [currentUser]);
+
+  return <div>hello</div>;
 };
 
 export default DashSaved;
