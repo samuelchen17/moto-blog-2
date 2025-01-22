cd81bf179fba11272d430508dddc88acf681f5a2
Samuel Chen <sche842@gmail.com>
2025-01-22 17:30:53 +1100

    fetch profile owners recent posts

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,13 +1,15 @@
 import { _get } from "@/api/axiosClient";
 import { Separator } from "@/components/ui/separator";
-import { IProfileData } from "@/types";
+import { IPostResponse, IProfileData } from "@/types";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { format } from "date-fns";
+import { Label } from "@/components/ui/label";
 
 const ProfilePage = () => {
   const { userId } = useParams<{ userId: string }>();
   const [profileData, setProfileData] = useState<IProfileData>();
+  const [postData, setPostData] = useState<IPostResponse>();
 
   useEffect(() => {
     const fetchProfile = async () => {
@@ -24,6 +26,26 @@ const ProfilePage = () => {
     fetchProfile();
   }, []);
 
+  // fetch posts if profile belongs to admin
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await _get<IPostResponse>(
+          `/post/getposts?createdBy=${userId}`
+        );
+        const data = res.data;
+
+        setPostData(data);
+      } catch (err) {
+        console.error("Error:", err);
+      }
+    };
+
+    fetchPosts();
+  }, [profileData]);
+
+  console.log(postData);
+
   if (profileData) {
     return (
       <div className="max-w-screen-xl mx-auto my-24 px-4">
@@ -37,27 +59,42 @@ const ProfilePage = () => {
             />
           </div>
 
-          <div>
-            <span>name: {profileData.username}</span>
-            <p>bio: </p>
-            {profileData.bio ? (
-              <p>{profileData.bio}</p>
-            ) : (
-              <p>No bio to display</p>
-            )}
+          <div className="flex flex-col space-y-5">
+            <div>
+              <Label>Name</Label>
+              <p>{profileData.username}</p>
+            </div>
 
-            <span>
-              Joined: {format(new Date(profileData.createdAt), "dd MMM yyyy")}
-            </span>
+            <div>
+              <Label>Bio</Label>
+              {profileData.bio ? (
+                <p>{profileData.bio}</p>
+              ) : (
+                <p>No bio to display</p>
+              )}
+            </div>
+            <div>
+              <Label>Joined</Label>{" "}
+              <div>
+                {format(new Date(profileData.createdAt), "dd MMM yyyy")}
+              </div>
+            </div>
           </div>
         </div>
 
-        {profileData.isAdmin && (
-          <div>
-            <h2>Recent Posts</h2>
-            <span>display user posts here</span>
-            if user is admin but no posts, show something here also,
-          </div>
+        <h2>Recent Posts</h2>
+        <Separator />
+        {profileData.isAdmin ? (
+          postData ? (
+            <div>
+              <span>display user posts here</span>
+              if user is admin but no posts, show something here also,
+            </div>
+          ) : (
+            <div>Skeleton here</div>
+          )
+        ) : (
+          <div>User is not have permission to post</div>
         )}
       </div>
     );
