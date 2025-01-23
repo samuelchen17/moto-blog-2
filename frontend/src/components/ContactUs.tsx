aa611516999af51c38ca0fd2ff716da72a0ac7f1
Samuel Chen <sche842@gmail.com>
2025-01-23 16:36:05 +1100

    contact ui minor adjustments

--- frontend/src/components/ContactUs.tsx
+++ frontend/src/components/ContactUs.tsx
@@ -5,14 +5,14 @@ import { Button } from "./ui/button";
 
 const ContactUs = () => {
   return (
-    <div className="flex justify-between overflow-hidden border rounded-md relative h-[450px]">
+    <div className="flex justify-between overflow-hidden md:border rounded-md relative md:h-[500px] h-[460px]">
       <img
-        className="absolute w-full h-full object-cover filter contrast-125 brightness-75 top-0 left-0"
+        className="absolute w-full h-full object-cover filter contrast-125 brightness-75 top-0 left-0 md:block hidden"
         alt="group ride image"
         src="https://images.unsplash.com/photo-1690540293122-14d3051a5fe5?q=80&w=2698&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
-      <div className="absolute bottom-0 top-0 outline flex flex-col md:flex-row w-full justify-end">
-        <div className="flex-grow space-y-6 rounded-lg p-6 bg-black bg-opacity-75 md:my-auto md:m-10 m-4 max-w-[600px]">
+      <div className="absolute bottom-0 top-0 flex flex-col md:flex-row w-full justify-end">
+        <div className="flex-grow space-y-6 rounded-lg p-6 bg-black md:bg-opacity-75 bg-opacity-90 md:my-auto md:m-8 max-w-[600px]">
           <div>
             <Label className="text-white">Name</Label>
             <Input
@@ -32,6 +32,7 @@ const ContactUs = () => {
             <Label className="text-white">Message</Label>
             <Textarea
               placeholder="your message..."
+              rows={5}
               className="bg-white  text-black border-black  dark:text-white  dark:bg-black dark:border-white"
             />
           </div>
