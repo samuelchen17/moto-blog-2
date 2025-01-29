ff4e33a42b1e34148ba6de0270c7e9df3fac7377
Samuel Chen <sche842@gmail.com>
2025-01-29 13:40:34 +1100

    contact us admin table cell formatting

--- frontend/src/components/dash/dashAdmin/dashMessages/columns.tsx
+++ frontend/src/components/dash/dashAdmin/dashMessages/columns.tsx
@@ -1,10 +1,19 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { IContactColumns } from "@/types";
+import { format } from "date-fns";
+import { Check, X } from "lucide-react";
 
 export const columns: ColumnDef<IContactColumns>[] = [
   {
     accessorKey: "createdAt",
     header: "Date",
+    cell: ({ row }) => {
+      const formattedDate = format(
+        new Date(row.getValue("createdAt")),
+        "d/MM/yy"
+      );
+      return <div>{formattedDate}</div>;
+    },
   },
   {
     accessorKey: "name",
@@ -21,5 +30,18 @@ export const columns: ColumnDef<IContactColumns>[] = [
   {
     accessorKey: "read",
     header: "Read",
+    cell: ({ row }) => {
+      const readStatus = row.getValue("read");
+
+      return (
+        <div className="">
+          {readStatus ? (
+            <Check className="text-green-600" />
+          ) : (
+            <X className="text-red-600" />
+          )}
+        </div>
+      );
+    },
   },
 ];
